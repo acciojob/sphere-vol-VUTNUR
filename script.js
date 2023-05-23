a9d21a015792717914869017fb556482b767a067
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-23 17:29:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function volume_sphere() {
     //Write your code here
-  let r=window.document.getElementById("radius").value
-	window.document.getElementById("volume").value=(4/3)*3.14*r*r*r;
+  let r=parseFloat(window.document.getElementById("radius").value);
+	window.document.getElementById("volume").value=(4/3)*3.14*(r*r*r);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;

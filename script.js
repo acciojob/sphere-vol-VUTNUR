e9e21bc8d6cda895a253f61dba46fd0b8b0dec30
Vutnur Nethaji Goud <130832771+VUTNUR@users.noreply.github.com>
2023-05-23 17:46:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function volume_sphere() {
     //Write your code here
+	event.preventDefault();
   let r=parseFloat(window.document.getElementById("radius").value);
 	window.document.getElementById("volume").value=(4/3)*Math.PI*Math.pow(r,3);
 } 
